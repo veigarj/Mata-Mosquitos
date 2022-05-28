68713b92ff53b5fcec8ce99e68002629263d19da
Glauco Veiga <veiga.cg@gmail.com>
2022-05-28 17:55:16 -0300

    1.1.3

--- jogo.js
+++ jogo.js
@@ -2,6 +2,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 // pega a altura e largura da janela do navegador
 function ajustaTamanhoPalcoJogo() {
@@ -11,6 +12,19 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if (tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		alert('Vitoria')
+	} else { 
+	document.getElementById('cronometro').innerHTML = tempo
+}
+}, 1000 )
 
 function posicaoRandomica() {
 	
