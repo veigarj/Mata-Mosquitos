74479f311c1d35cfccde84f4802b8b8fedf955c8
Glauco Veiga <veiga.cg@gmail.com>
2022-05-28 18:33:04 -0300

    1.1.4

--- jogo.js
+++ jogo.js
@@ -20,6 +20,7 @@ var cronometro = setInterval(function() {
 	if (tempo < 0) {
 		clearInterval(cronometro)
 		clearInterval(criaMosquito)
+		window.location.href = 'vitoria.html'
 		alert('Vitoria')
 	} else { 
 	document.getElementById('cronometro').innerHTML = tempo
